1b8bd38016ca1a7beda6cd81f2b87dc26b76a652
Stefan Köhnen <stefan.khnen@googlemail.com>
2015-03-21 00:15:14 +0100

    Added curl calls

--- main.cpp
+++ main.cpp
@@ -5,12 +5,26 @@
 int main( ) {
 
   CURL* curl;
+  CURLcode response;
 
+  std::ifstream urlFile( "url" );
+  
   // Initialize curl
   curl = curl_easy_init();
 
   // Check if curl initialized correctly
   if( curl ) {
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
+    
+    curl_easy_setopt( curl, CURLOPT_URL, "" );
+
+#pragma clang diagnostic pop
+    
+    response = curl_easy_perform( curl );
+
+    curl_easy_cleanup( curl );
     
   } else {
     std::cerr << "Curl init error" << std::endl;
